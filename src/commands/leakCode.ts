054a918fd70c3fe7457e00734efd345e1f010b46
Ketok <ketok4321@gmail.com>
2023-04-06 15:40:42 +0200

    Remove unnecessary double null check

--- src/commands/leakCode.ts
+++ src/commands/leakCode.ts
@@ -5,8 +5,9 @@ export const leakCodeCommand = new CommandBuilder()
     .withDescription("Najbardziej user-friendly komenda do sprawdzania co robi jakaś komenda :thumbsup:")
     .executes(({ bot, message }, command) => {
         const [cmd, _] = bot.commandHandler.getCommand(command) ?? [null, null];
+
         if (!cmd) return void message.reply("Ta komenda nie istnieje");
         if (cmd instanceof Array) return void message.reply("Leakowanie kodu grup komend nie jest jeszcze supportowane przez pata");
-        if (!cmd) return void message.reply("nie ma takiej komendy");
+
         message.reply("(kod poniżej jest wynikiem kompilacji z ts do js, więc różni się nieco od prawdziwego)\n```js\n" + cmd.execute.toString() + "```");
     });
